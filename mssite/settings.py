4e4b89965a8e73af327550e6005f49f6f6b31507
zormnus <zotov.max17@mail.ru>
2024-02-21 13:35:11 +0300

    static deploy fixes 1

--- mssite/settings.py
+++ mssite/settings.py
@@ -128,7 +128,8 @@ AUTH_USER_MODEL = "users.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
