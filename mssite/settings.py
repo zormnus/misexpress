c96723fea4873f69ce8946883c6b4176bd30593d
zormnus <zotov.max17@mail.ru>
2024-02-25 18:36:22 +0300

    small fix

--- mssite/settings.py
+++ mssite/settings.py
@@ -33,8 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env("DEBUG")
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
