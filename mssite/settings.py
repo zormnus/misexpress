6ffd8cc79575b9fc32cc0746fb7d5c0689400849
zormnus <zotov.max17@mail.ru>
2024-03-16 15:12:48 +0300

    small fixes

--- mssite/settings.py
+++ mssite/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
-INTERNAL_IPS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
